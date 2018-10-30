016eb6796ba5ecb977a227e9a2801a9f8d3c744f
nleitefaria <nleitefaria@gmail.com>
2018-10-30 11:33:24 +0000

    Refactor

--- src/components/Financials.js
+++ src/components/Financials.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
-class Financials extends Component {
+class Financials extends Component
+{
 	  render() {
 	    return (
 	      <div>
-	        <h2>Financials</h2>	        
+	        <h2>Financials</h2>	  
+	      
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	        
 	      </div>
+	      
+	      
+	      
+	      
+	      
+	      
+	      
 	    );
 	  }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,29 +3,45 @@ import {
 	  Route,
 	  NavLink,
 	  HashRouter
-	} from "react-router-dom";
+} from "react-router-dom";
+import {
+	  Container, Row, Col
+} from 'reactstrap';
 
 import Home from './Home';
 import Financials from './Financials';
+import Profile from './Profile';
 	
 
-class Main extends Component {
+class Main extends Component 
+{
 	  render() {
 	    return (
 	    		<HashRouter>
-	            <div>
-	              <ul className="header">
-	                <li><NavLink to="/">Home</NavLink></li>
-	                <li><NavLink to="/financials">Financials</NavLink></li>
-	                
-	              </ul>
-	              <div className="content">
-	              	<Route exact path="/" component={Home}/>
-	                <Route path="/financials" component={Financials}/>
-	                
-	              </div>
-	            </div>
-	          </HashRouter>
+	            	<div>
+	            		<div>								
+	            			<Container>
+	            				<Row>
+	            					<Col>
+	            						<ul className="header">
+	            							<li><NavLink to="/">Home</NavLink></li>
+	            							<li><NavLink to="/financials">Financials</NavLink></li>
+	            							<li><NavLink to="/profile">Profile</NavLink></li>
+	            						</ul>
+	            						<div className="content">
+	            							<Route exact path="/" component={Home}/>
+	            							<Route path="/financials" component={Financials}/>
+	            							<Route path="/profile" component={Profile}/>
+	            						</div>
+	              	
+	              	
+              	
+	            					</Col>
+	            				</Row>
+	            			</Container>
+	            		</div>  	
+	            	</div>
+	            </HashRouter>
 	    );
 	  }
 }
