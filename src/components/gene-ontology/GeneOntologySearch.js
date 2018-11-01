40484b344c6dfeabfc58260a94ffd84c667b1f38
nleitefaria <nleitefaria@gmail.com>
2018-11-01 10:06:33 +0000

    added form to GeneOntologySearch

--- src/components/gene-ontology/GeneOntologySearch.js
+++ src/components/gene-ontology/GeneOntologySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Form, Label, Input } from 'reactstrap';
 
 class GeneOntologySearch extends Component
 {	
@@ -6,47 +7,97 @@ class GeneOntologySearch extends Component
 	{
 	    super(props);
 	    this.state = {
+	      inputvalue: '',
 	      error: null,
-	      isLoaded: false,
+	      //isLoaded: false,
 	      numberOfHits: null,
 	      results: []
 	    };
+	    this.handleChange = this.handleChange.bind(this);
+	    this.handleSubmit = this.handleSubmit.bind(this);
+	    
 	  }
 
-	  componentDidMount() {
-	    fetch("https://www.ebi.ac.uk/QuickGO/services/ontology/go/search?query=hi&limit=25&page=1")
+	  componentDidMount() {	    
+	  }
+	  
+	  fetchSearch(param)
+	  {		  
+		  fetch('https://www.ebi.ac.uk/QuickGO/services/ontology/go/search?query=' + param + '&limit=25&page=1')
 	      .then(res => res.json())
 	      .then(
 	        (result) => {
 	          this.setState({
-	            isLoaded: true,
+	            //isLoaded: true,
 	            numberOfHits: result.numberOfHits,
 	  	        results: result.results
 	          });
 	        },
 	        (error) => {
 	          this.setState({
-	            isLoaded: true,
+	            //isLoaded: true,
 	            error
 	          });
 	        }
-	      )
+	      )  
+	  }
+	  
+	  handleChange (event) 
+	  {
+		    this.setState({
+		        inputvalue: event.target.value
+		    })
+	  }
+	  
+	  handleSubmit (event) 
+	  {
+		  this.fetchSearch(this.state.inputvalue);
+	      console.log('Form value: ' + this.state.inputvalue);
+	      event.preventDefault();
 	  }
 
-	  render() {
-	    const { error, isLoaded, numberOfHits, results } = this.state;
-	    if (error) {
-	      return <div>Error: {error.message}</div>;
-	    } else if (!isLoaded) {
-	      return <div>Loading...</div>;
-	    } else {
-	      return (
-	        <div>        	        
-	        {numberOfHits}
-	        <br></br>
-	        {results.length}
-	        </div>
-	      );
+	  render() 
+	  {
+		  const { error, numberOfHits, results } = this.state;
+		  if (error) 
+		  {
+			  return <div>Error: {error.message}</div>;
+		  } 
+		  else 
+		  {
+			  if(results.length !== 0)
+			  {
+				  return (
+	    		        <div>        	        	        	        
+	    		        	<Form onSubmit={this.handleSubmit}>
+	    	            		<Label>Query</Label>
+	    	            		<Input type="text" value={this.state.inputvalue} onChange={this.handleChange}/>           		
+	    	            		<br></br>
+	    	            		<Button type="submit">Search</Button>         		
+	    	            	</Form>
+	    	            		
+	    	            	{numberOfHits}
+	    	    	        <br></br>
+	    	    	        {results.length}
+	    	            		
+	    	            		
+	    		        
+	    		        </div>
+				  );
+    		}
+	    	else
+	    	{
+	    		return (
+	    		        <div>        	        	        	        
+	    		        	<Form onSubmit={this.handleSubmit}>
+	    	            		<Label>Query</Label>
+	    	            		<Input type="text" value={this.state.inputvalue} onChange={this.handleChange}/>           		
+	    	            		<br></br>
+	    	            		<Button type="submit">Search</Button>         		
+	    	            	</Form>    
+	    		        </div>
+	    		      );
+	    	}
 	    }
 	  }
 }
