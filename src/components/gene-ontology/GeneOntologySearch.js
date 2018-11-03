46971b36e5773bb986eacb4f6274327eca3590e0
nleitefaria <nleitefaria@gmail.com>
2018-11-03 15:16:13 +0000

    Added details to GO search

--- src/components/gene-ontology/GeneOntologySearch.js
+++ src/components/gene-ontology/GeneOntologySearch.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { Card, Form, Input } from 'reactstrap';
 import { PagingState, CustomPaging} from '@devexpress/dx-react-grid';
 import { Grid, Table, TableHeaderRow, PagingPanel} from '@devexpress/dx-react-grid-bootstrap4';
+import GeneOntologyTermsModal from './GeneOntologyTermsModal';
 
 const URL = 'https://www.ebi.ac.uk/QuickGO/services/ontology/go/search';
+
+const ActionCell = ({ id }) => (
+  <Table.Cell>
+    <span>
+				<GeneOntologyTermsModal id={id}/>
+    </span>
+  </Table.Cell>
+);
+
+const Cell = (props) => {
+  const { column, row } = props;
+  if (column.name === 'action') {
+    return <ActionCell  id={row.id} />;
+  }
+  return <Table.Cell {...props} />;
+};
 
 class GeneOntologySearch extends React.PureComponent
 {
@@ -19,16 +36,19 @@ class GeneOntologySearch extends React.PureComponent
 	          { name: 'id', title: 'ID' },
 	          { name: 'name', title: 'Name'},
 	          { name: 'aspect', title: 'Aspect' },
+						{ name: "action", title: "Action" }
 	        ],
 	        rows: [],
 	        totalCount: 0,
 	        pageSize: 5,
 	        currentPage: 0,
 	        loading: true,
+					modal: false
 	    };
 	    this.handleChange = this.handleChange.bind(this);
 	    this.handleSubmit = this.handleSubmit.bind(this);
 	    this.changeCurrentPage = this.changeCurrentPage.bind(this);
+			this.toggle = this.toggle.bind(this);
 
 	  }
 
@@ -43,18 +63,18 @@ class GeneOntologySearch extends React.PureComponent
 	  }
 
 
-	  handleChange (event)
-	  {
-		    this.setState({
-		        inputvalue: event.target.value
-		    })
-	  }
+		handleChange (event)
+	 {
+			 this.setState({
+					 inputvalue: event.target.value
+			 })
+	 }
 
-	  handleSubmit (event)
-	  {
-		  this.loadData(this.state.inputvalue);
-	      event.preventDefault();
-	  }
+	 handleSubmit (event)
+	 {
+		   this.loadData(this.state.inputvalue);
+			 event.preventDefault();
+	 }
 
 	  changeCurrentPage(currentPage)
 	  {
@@ -63,6 +83,13 @@ class GeneOntologySearch extends React.PureComponent
 		      currentPage: currentPage,
 		    });
 		}
+
+		toggle()
+		{
+    	this.setState({
+      	modal: !this.state.modal
+    	});
+  	}
 
 
 	  queryString()
@@ -138,9 +165,9 @@ class GeneOntologySearch extends React.PureComponent
 	    	    	        	<Grid rows={rows} columns={columns} >
 	    	    	        		<PagingState currentPage={currentPage} onCurrentPageChange={this.changeCurrentPage} pageSize={pageSize} />
 	    	    	        			<CustomPaging totalCount={totalCount} />
-	    	    	          				<Table />
-	    	    	          					<TableHeaderRow />
-	    	    	          						<PagingPanel />
+															<Table cellComponent={Cell}/>
+																<TableHeaderRow />
+																	<PagingPanel />
 	    	    	          	</Grid>
 	    	    	        </Card>
 	    		        </div>
--- src/components/gene-ontology/GeneOntologyTerms.js
+++ src/components/gene-ontology/GeneOntologyTerms.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Card, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Card } from 'reactstrap';
 import { PagingState, CustomPaging} from '@devexpress/dx-react-grid';
 import { Grid, Table, TableHeaderRow, PagingPanel} from '@devexpress/dx-react-grid-bootstrap4';
 import GeneOntologyTermsModal from './GeneOntologyTermsModal';
 
 const URL = 'https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms';
 
-const HighlightedCell = ({ id }) => (
+const ActionCell = ({ id }) => (
   <Table.Cell>
     <span>
 				<GeneOntologyTermsModal id={id}/>
@@ -17,7 +17,7 @@ const HighlightedCell = ({ id }) => (
 const Cell = (props) => {
   const { column, row } = props;
   if (column.name === 'action') {
-    return <HighlightedCell  id={row.id} />;
+    return <ActionCell  id={row.id} />;
   }
   return <Table.Cell {...props} />;
 };
@@ -138,16 +138,6 @@ class GeneOntologyTerms extends React.PureComponent
 	    	    	          							<PagingPanel />
 	    	    	          	</Grid>
 	    	    	        </Card>
-											<Modal>
-											<ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
 	    		        </div>
 	    		      );
 	    }
