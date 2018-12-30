316cb7e54b1dec796d953f25b6d83499a9870c4a
nleitefaria <nleitefaria@gmail.com>
2018-12-30 20:56:06 +0000

    Added Search and Terms to EvidenceConclusionOntology

---
+++ src/components/evidence-conclusion-ontology/EvidenceConclusionOntologySearch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class EvidenceConclusionOntologySearch extends React.Component 
+{
+    render() {
+          return (
+                  <div>
+                    TODO Search
+                  </div>
+                )
+        }
+}
+
+export default EvidenceConclusionOntologySearch;
---
+++ src/components/evidence-conclusion-ontology/EvidenceConclusionOntologyTerms.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class EvidenceConclusionOntologyTerms extends React.Component 
+{
+    render() {
+          return (
+                  <div>
+                    TODO Terms
+                  </div>
+          )
+    }
+}
+
+export default EvidenceConclusionOntologyTerms;
